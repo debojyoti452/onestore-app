62d67d3d5c8b9f1c8da9bcf288e4497f7c89a3bd
Debojyoti Singha <debojyoti@aeologic.com>
2023-01-05 02:48:50 +0530

    first-ui-done

--- lib/src/my_wallet_screen.dart
+++ lib/src/my_wallet_screen.dart
@@ -32,6 +32,7 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
           borderRadius: BorderRadius.circular(10),
         ),
         mini: true,
+        backgroundColor: const Color(0xff5660AF),
         child: Icon(
           Icons.add_circle_outline_rounded,
           size: 30,
@@ -60,36 +61,33 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          margin: const EdgeInsets.only(bottom: 70),
-          child: ListView.builder(
-            controller: controller,
-            shrinkWrap: true,
-            itemCount: 20,
-            scrollDirection: Axis.vertical,
-            physics: const BouncingScrollPhysics(),
-            itemBuilder: (context, index) {
-              double scale = 1.0;
-              if (topContainer > 0.5) {
-                scale = index + 0.5 - topContainer;
-                if (scale < 0) {
-                  scale = 0;
-                } else if (scale > 1) {
-                  scale = 1;
-                }
-              }
-              return Opacity(
-                opacity: scale,
-                child: Transform(
-                  transform: Matrix4.identity()..scale(scale, scale),
-                  alignment: Alignment.bottomCenter,
-                  child: Align(
-                    heightFactor: 0.8,
-                    alignment: Alignment.topCenter,
-                    child: _creditCardUI(),
-                  ),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  controller: controller,
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  scrollDirection: Axis.vertical,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    if (index == 9) {
+                      return const SizedBox(
+                        height: 80,
+                      );
+                    }
+                    return Align(
+                      heightFactor: 0.9,
+                      alignment: Alignment.topCenter,
+                      child: _creditCardUI(),
+                    );
+                  },
                 ),
-              );
-            },
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+            ],
           ),
         ),
       ),
@@ -99,16 +97,16 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
   /// Credit Card UI
   Widget _creditCardUI() {
     return Container(
-      height: 180,
-      margin: const EdgeInsets.only(bottom: 20),
-      padding: const EdgeInsets.all(20),
+      height: 200,
+      padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 20),
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withAlpha(100),
-            blurRadius: 10.0,
+            blurRadius: 6.0,
             spreadRadius: 2.0,
           ),
         ],
@@ -127,62 +125,51 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
         children: [
           Row(
             children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Icon(
-                  Icons.credit_card,
-                  color: Colors.white,
-                ),
-              ),
               const SizedBox(width: 20),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
+                children: [
                   Text(
-                    'Credit Card',
+                    'ICICI Bank',
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                      color: Colors.white.withOpacity(0.8),
                     ),
                   ),
-                  SizedBox(height: 5),
+                  const SizedBox(height: 50),
                   Text(
-                    '**** **** **** 1234',
+                    '4520 **** **** 1234',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white,
+                      color: Colors.white.withOpacity(0.8),
                     ),
+                  ),
+                  const SizedBox(height: 40),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Debojyoti Singha',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                      const SizedBox(width: 80),
+                      Text(
+                        'Valid Thru: 12/22',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
-              ),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: const [
-              Text(
-                'Balance',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                '\$ 1,234.00',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
               ),
             ],
           ),
